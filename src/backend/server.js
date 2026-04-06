7005b3d5d95c533c4cea199a0611a51079f41fd7
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2026-04-05 23:43:52 -0700

    fixing where its pointing

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 
 const app = express();
-const PORT = 3001;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
     res.send('backend is running!')
 })
 
 app.listen(PORT, () => {
-    console.log(`app is listening on port: http://localhost:${PORT}`)
+    console.log(`app is listening on port: http://localhost:${port}`)
 })

ed40dc156a3efa555ce84e2eca1abb1dc0cc080d
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2026-04-05 23:45:33 -0700

    fix

--- src/backend/server.js
+++ src/backend/server.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
     res.send('backend is running!')
 })
 
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log(`app is listening on port: http://localhost:${port}`)
 })
